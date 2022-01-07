91f208396b94a6a6ef965d52a2dfb53b2d43f078
Hiroki Ishiwatari <dev.ish1r0k1@gmail.com>
2022-01-07 17:04:17 +0900

    Update

--- install.sh
+++ install.sh
@@ -10,3 +10,22 @@ do
   [ "$f" = ".DS_Store" ] && continue
   ln -snfv "$DOT_FILES/$f" "$HOME"/"$f"
 done
+
+# for fish
+cd $DOT_FILES/fish
+
+for f in *
+do
+  ln -snfv "$(pwd)/$f" "$HOME/.config/fish/$f"
+done
+
+cd -
+
+# for vim
+# cd $HOME/.vim
+
+# for f in *
+# do
+#   ln -snfv "$(pwd)/$f" "$HOME/.config/nvim/$f"
+# done
+ln -snfv ~/.vimrc ~/.config/nvim/init.vim
